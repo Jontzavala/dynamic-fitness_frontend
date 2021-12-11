02aeecf9c82798088f670ddea8554861a01bdb74
Jonathan Zavala <jontzavala@gmail.com>
2021-12-11 16:11:27 -0600

    set up store with provider and set up router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import {Provider} from 'react-redux';
+import {BrowserRouter as Router} from 'react-router-dom';
+import gymsReducer from './reducers/gymReducer.js'
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(gymsReducer, composeEnhancer(applyMiddleware(thunk)));
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App/>
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
