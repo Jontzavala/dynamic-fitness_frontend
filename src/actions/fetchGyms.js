6386302b7b4166918cdfde942887c3254526c80c
Jonathan Zavala <jontzavala@gmail.com>
2021-12-11 16:22:04 -0600

    added the fetch request for all gyms

---
+++ src/actions/fetchGyms.js
@@ -0,0 +1,14 @@
+export function fetchGyms() {
+    return (dispatch) => {
+        fetch('http://localhost:3000/api/v1/gyms')
+        .then(response => response.json())
+        .then(gyms => {
+            if(gyms.error) {
+                alert(books.error)
+            }else{
+                dispatch({type: 'FETCH_GYMS', payload: gyms.data})
+            }
+        })
+        .catch(err => alert(err));
+    }
+}
