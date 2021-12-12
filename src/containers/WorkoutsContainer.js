ad9ff31a0e61776185503f888f7591831a558907
Jonathan Zavala <jontzavala@gmail.com>
2021-12-11 21:34:55 -0600

    added what i need in the workouts container

--- src/containers/WorkoutsContainer.js
+++ src/containers/WorkoutsContainer.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
 import Workouts from '../components/Workouts.js'
+import WorkoutsCreateForm from '../components/WorkoutsCreateForm'
+import {Route, Switch} from 'react-router-dom'
+import SuccessModal from '../components/SuccessModal'
 
 class WorkoutsContainer extends Component {
+
+    state = {
+        showModal: false
+    }
+
+    toggleModal = () => {
+        this.setState({ showModal: this.state.showModal })
+    }
+
+
     render() {
         return (
             <div>
-                <Workouts workouts={this.props.gym[0].attributes.workouts}/> 
+                <Switch>
+                    <Route path='/gyms/:id/workouts/new' render={(routeProps) => 
+                    <WorkoutsCreateForm {...routeProps} gym={this.props.gym} toggle={this.toggleModal}/>}/>
+                </Switch>
+                <Workouts workouts={this.props.gym[0].attributes.workouts}/>
+                <SuccessModal isOpen={this.state.showModal} toggle={this.toggleModal}/>
             </div>
         )
     }
