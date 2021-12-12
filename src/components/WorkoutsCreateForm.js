8eb83e4ec785291d25373ff9cfd26bd1cb201ff5
Jonathan Zavala <jontzavala@gmail.com>
2021-12-11 21:23:15 -0600

    filled in workouts createform

--- src/components/WorkoutsCreateForm.js
+++ src/components/WorkoutsCreateForm.js
@@ -5,11 +5,44 @@ import {Form, Button, Container} from 'react-bootstrap'
 import WorkoutsContainer from '../containers/WorkoutsContainer'
 
 class WorkoutsCreateForm extends Component {
+
+    state = {
+        title: '',
+        content: ''
+    }
+
+    handleOnChange = (event) => {
+        this.setState({
+          [event.target.name] : event.target.value
+        })
+    }
+      
+      
+    handleOnSubmit = (event) => {
+        event.preventDefault();
+        this.props.addWorkout(this.state, this.props.gym[0].id, this.props.history);
+        this.props.toggle();
+    }
+
+
     render() {
         return (
-            <div>
-                
-            </div>
+            <Container>
+                <h2>Create a Workout</h2>
+                <Form onSubmit={this.handleOnSubmit}>
+                    <Form.Group controlId="title">
+                        <Form.Label>Workout Title:</Form.Label>
+                        <Form.Control type="text" name="title" value={this.state.title} onChange={this.handleOnChange} required/>
+                    </Form.Group>
+                    <Form.Group controlId="content">
+                        <Form.Label>Content</Form.Label>
+                        <Form.Control type="text" name="content" value={this.state.content} onChange={this.handleOnChange} required/>
+                    </Form.Group>
+                    <div>
+                        <Button type="submit">Submit</Button>
+                    </div>
+                </Form>
+            </Container>
         )
     }
 }
