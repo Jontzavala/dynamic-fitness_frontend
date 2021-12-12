626587b54b3a1db06168ec50ec02ed5f7e8edb0e
Jonathan Zavala <jontzavala@gmail.com>
2021-12-11 19:13:22 -0600

    created gymshow component

---
+++ src/components/GymShow.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {useParams, Link} from 'react-router-dom'
+import {Container, Row, Col, Button} from 'react-bootstrap';
+import {connect} from 'react-redux'
+
+const GymShow = (props) => {
+    
+}
