bb1d58f47f5d4e365f4f8cf6620a72e99d200139
Jonathan Zavala <jontzavala@gmail.com>
2021-12-11 22:17:25 -0600

    added delete to workout js

--- src/components/Workouts.js
+++ src/components/Workouts.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {Container, Card} from 'react-bootstrap'
+import {Container, Card, Button} from 'react-bootstrap'
+import {deleteWorkout} from '../actions/deleteWorkout'
 
 const Workouts = (props) => {
+
+    const handleDelete = (workout) => {
+        props.deleteWorkout(workout.id, workout.gym_id)
+    }
+
     return(
         <Container>
             <h2>Workouts</h2>
@@ -10,9 +16,10 @@ const Workouts = (props) => {
                 <Card key={workout.id}>
                     <Card.Title>{workout.title}</Card.Title>
                     <Card.Text>{workout.content}</Card.Text>
+                    <Button onClick={() => handleDelete(workout)}>Delete Workout</Button>
                 </Card>)}
         </Container>
     )
 }
 
-export default connect()(Workouts)
+export default connect(null, {deleteWorkout})(Workouts)
